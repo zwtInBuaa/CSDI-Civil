974909c526a35256c83c235c9f1147ae426112d9
朱文涛 <20373915@buaa.edu.cn>
2023-12-20 12:04:59 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -108,8 +108,8 @@ class ResidualBlock(nn.Module):
         self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
         self.output_projection = Conv1d_with_init(channels, 2 * channels, 1)
 
-        self.time_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
-        self.feature_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+        self.time_layer = get_torch_trans(heads=nheads, layers=2, channels=channels)
+        self.feature_layer = get_torch_trans(heads=nheads, layers=2, channels=channels)
 
         self.transformer_layer = get_torch_trans(heads=nheads, layers=2, channels=channels)
 
@@ -188,7 +188,7 @@ class ResidualBlock(nn.Module):
         # y1 = self.forward_time(y, base_shape)
         # y2 = self.forward_feature(y, base_shape)
         # y = self.forward_combined((y1+y2)/2,base_shape)
-        y = self.forward_combined(y, base_shape)
+        # y = self.forward_combined(y, base_shape)
         # print("y2:")
         # print(y, y.shape)
         # y = self.forward_transformer(y, base_shape)
