5f4f29f601f5b6c62c8fd02aaede7f7b91897e6c
朱文涛 <20373915@buaa.edu.cn>
2023-12-21 10:30:41 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -161,8 +161,8 @@ class ResidualBlock(nn.Module):
         # x = x.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
         # dec_out = self.projection(enc_out)
         y = y.reshape(B, channel, K, L).reshape(B, channel, K * L)
-        y = self.transformer_layer(y.permute(2, 0, 1))
-        print(y)
+        y, attens = self.transformer_layer(y.permute(2, 0, 1))
+        y = y.permute(1, 2, 0)
         y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
         return y
 
