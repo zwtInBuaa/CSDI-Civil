c992d4e1350ccce1a003274d0f449831d3b18d43
朱文涛 <20373915@buaa.edu.cn>
2023-12-22 18:54:20 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -204,8 +204,6 @@ class ResidualBlock(nn.Module):
         diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
         y = x + diffusion_emb
 
-        # y = self.transformer_layer(y, base_shape)
-
         y = self.forward_time(y, base_shape)
 
         # # # print("y1:")
