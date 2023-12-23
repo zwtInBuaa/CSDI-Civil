5ab3a06a94a4321ae0677ca50f080ddf83a1d907
朱文涛 <20373915@buaa.edu.cn>
2023-12-24 02:48:45 +0800

    ""

--- exe_fcs.py
+++ exe_fcs.py
@@ -29,12 +29,8 @@ parser = argparse.ArgumentParser(description="CSDI")
 parser.add_argument("--config", type=str, default="base.yaml")
 parser.add_argument('--device', default='cuda:0', help='Device for Attack')
 parser.add_argument("--modelfolder", type=str, default="")
-parser.add_argument(
-    "--targetstrategy", type=str, default="mix", choices=["mix", "random", "historical"]
-)
-parser.add_argument(
-    "--validationindex", type=int, default=0, help="index of month used for validation (value:[0-7])"
-)
+parser.add_argument("--targetstrategy", type=str, default="mix", choices=["mix", "random", "historical"])
+parser.add_argument("--validationindex", type=int, default=0, help="index of month used for validation (value:[0-7])")
 parser.add_argument("--nsample", type=int, default=100)
 parser.add_argument("--unconditional", action="store_true")
 
@@ -51,9 +47,7 @@ config["model"]["target_strategy"] = args.targetstrategy
 print(json.dumps(config, indent=4))
 
 current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-foldername = (
-        "./save/pm25_validationindex" + str(args.validationindex) + "_" + current_time + "/"
-)
+foldername = ("./save/pm25_validationindex" + str(args.validationindex) + "_" + current_time + "/")
 
 print('model folder:', foldername)
 os.makedirs(foldername, exist_ok=True)
