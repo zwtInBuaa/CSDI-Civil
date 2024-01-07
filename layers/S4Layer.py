d53824eadd2ad5e223f2d150c836e276f615e8c9
朱文涛 <20373915@buaa.edu.cn>
2024-01-07 16:49:34 +0800

    ""

--- layers/S4Layer.py
+++ layers/S4Layer.py
@@ -1,19 +1,9 @@
 import numpy as np
 import random
 import torch
-from torch.optim import Adam
-from tqdm import tqdm
-import pickle
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-import argparse
-import datetime
-import json
-import yaml
-import os
-from torch.utils.data import DataLoader, Dataset
-import wandb
 import logging
 from functools import partial
 from scipy import special as ss
