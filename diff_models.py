a5e146f3061ec2889d834159ebe55164d459b772
朱文涛 <20373915@buaa.edu.cn>
2024-01-11 11:30:32 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -91,16 +91,12 @@ class diff_CSDI(nn.Module):
         for layer in self.residual_layers:
             x, skip_connection = layer(x, cond_info, diffusion_emb)
             skip.append(skip_connection)
-        x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
+        # x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
         skip = []
-        i = 0
-        for layer in self.residual_layers:
-            if i == 3:
-                break
+        for layer in reversed(self.residual_layers):
             x, skip_connection = layer(x, cond_info, diffusion_emb)
             skip.append(skip_connection)
-            i += 1
-        x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(4)
+        x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers) * 2)
 
         x = x.reshape(B, self.channels, K * L)
         x = self.output_projection1(x)  # (B,channel,K*L)
