9b95d671499d0e07a0d77a4c3e5c52c34852b9a5
朱文涛 <20373915@buaa.edu.cn>
2024-01-12 01:21:41 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -141,17 +141,16 @@ class ResidualBlock(nn.Module):
         x = x.reshape(B, channel, K * L)
 
         y = x
-
         y = self.s4_init_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
 
-        y_time = self.forward_time(y, base_shape)
-        # y_time = self.forward_time(y, base_shape)
-        y_feature = self.forward_feature(y, base_shape)  # (B,channel,K*L)
-        y = torch.sigmoid(y_time) * torch.tanh(y_feature)
         # y = self.mid_projection(y)  # (B,2*channel,K*L)
 
         diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
         y = y + diffusion_emb
+        y_time = self.forward_time(y, base_shape)
+        # y_time = self.forward_time(y, base_shape)
+        y_feature = self.forward_feature(y, base_shape)  # (B,channel,K*L)
+        y = torch.sigmoid(y_time) * torch.tanh(y_feature)
 
         y = self.mid_projection(y)
 
