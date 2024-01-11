797e70d7c37df9b2eccec61c8dfb64a7fdc04581
朱文涛 <20373915@buaa.edu.cn>
2024-01-11 16:30:23 +0800

    ""

--- layers/S4Improved.py
+++ layers/S4Improved.py
@@ -1498,3 +1498,27 @@ class S4(nn.Module):
     @property
     def d_output(self):
         return self.H
+
+
+class S4Layer(nn.Module):
+    '''S4 Layer that can be used as a drop-in replacement for a TransformerEncoder'''
+
+    def __init__(self, features, lmax, N=64, dropout=0.0, bidirectional=True, layer_norm=True):
+        super().__init__()
+        self.s4_layer = S4(d_model=features,
+                           d_state=N,
+                           l_max=251,
+                           bidirectional=True)
+
+        self.norm_layer = nn.LayerNorm(features) if layer_norm else nn.Identity()
+        self.dropout = nn.Dropout2d(dropout) if dropout > 0 else nn.Identity()
+
+    def forward(self, x):
+        # x has shape seq, batch, feature
+        xin = x.permute((1, 2, 0))  # batch, feature, seq (as expected from S4 with transposed=True)
+
+        xout, _ = self.s4_layer(xin)  # batch, feature, seq
+        xout = self.dropout(xout)
+        xout = xout + xin  # skip connection   # batch, feature, seq
+        xout = xout.permute((2, 0, 1))  # seq, batch, feature
+        return self.norm_layer(xout)
