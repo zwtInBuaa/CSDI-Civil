290e5dc535d75fd2c9c6df01d6626aa12df6fa5a
朱文涛 <20373915@buaa.edu.cn>
2024-01-13 23:30:49 +0800

    ""

--- diff_models.py
+++ diff_models.py
@@ -129,7 +129,7 @@ class ResidualBlock(nn.Module):
     def __init__(self, side_dim, channels, diffusion_embedding_dim, nheads):
         super().__init__()
         self.diffusion_projection = nn.Linear(diffusion_embedding_dim, channels)
-        self.diffusion_conv = Conv1d_with_init(channels, 2 * channels)
+        self.diffusion_conv = Conv1d_with_init(channels, 2 * channels, 1)
 
         self.cond_projection = Conv1d_with_init(side_dim, 2 * channels, 1)
         self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
