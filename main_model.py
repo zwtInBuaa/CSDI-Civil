069a8f9468f393a3369f8b6457f7eef4327f86b1
朱文涛 <20373915@buaa.edu.cn>
2024-03-23 17:04:58 +0800

    ""

--- main_model.py
+++ main_model.py
@@ -162,8 +162,8 @@ class CSDI_base(nn.Module):
             if not self.use_guide:
                 total_input = torch.cat([cond_obs, noisy_target], dim=1)  # (B,2,K,L)
             else:
-                tmp_data = torch.tensor(observed_data).to(torch.float64)
-                itp_data = torch.where(cond_mask == 0, float('nan'), tmp_data).to(torch.float64)
+                itp_data = torch.tensor(observed_data).to(torch.float64)
+                # itp_data = torch.where(cond_mask == 0, float('nan'), tmp_data).to(torch.float64)
                 itp_data = torchcde.linear_interpolation_coeffs(itp_data.permute(2, 1), rectilinear=0).permute(2, 1)
                 itp_data = torchcde.LinearInterpolation(itp_data).unsqueeze(1)
                 total_input = torch.cat([itp_data, noisy_target], dim=1)  # (B,2,K,L)
