0fd58090ac38bd78d77971a224f108270a6f3d94
朱文涛 <20373915@buaa.edu.cn>
2024-05-03 03:24:05 +0800

    ""

--- main_model.py
+++ main_model.py
@@ -42,7 +42,7 @@ class CSDI_base(nn.Module):
         elif self.diff_model == 2:
             self.diffmodel = diff_CSDI_saits(config_diff, input_dim)
         elif self.diff_model == 3:
-            self.diffmodel = diff_dilated_conv(config_diff, input_dim)
+            self.diffmodel = diff_dilated_conv(config_diff, input_dim) 
 
         # parameters for diffusion models
         self.num_steps = config_diff["num_steps"]
