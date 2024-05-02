6a69d7ce50a207e4bfd768315feab0d11251db81
朱文涛 <20373915@buaa.edu.cn>
2024-05-03 04:18:27 +0800

    ""

--- diffmodel/diff_tcn.py
+++ diffmodel/diff_tcn.py
@@ -104,16 +104,7 @@ class ResidualBlock(nn.Module):
         self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
         self.output_projection = Conv1d_with_init(channels, 2 * channels, 1)
 
-        self.time_layer = get_tcn(input_size=channels, hidden_size=[144, 288, 576])
-
-    def forward_time(self, y, base_shape):
-        B, channel, K, L = base_shape
-        if L == 1:
-            return y
-        y = y.reshape(B, channel, K, L).permute(0, 2, 1, 3).reshape(B * K, channel, L)
-        y = self.time_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
-        y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
-        return y
+        self.time_layer = get_tcn(input_size=channels, hidden_size=[16, 32, 64])
 
     def forward(self, x, cond_info, diffusion_emb):
         B, channel, K, L = x.shape
@@ -123,7 +114,7 @@ class ResidualBlock(nn.Module):
         diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
         y = x + diffusion_emb
 
-        y = self.forward_time(y, base_shape)
+        y = self.time_layer(y)
         y = self.mid_projection(y)  # (B,2*channel,K*L)
 
         _, cond_dim, _, _ = cond_info.shape
