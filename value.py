6b8d31b9c6de0b2a4f2a0b558d63de86e0d9eccf
朱文涛 <20373915@buaa.edu.cn>
2024-05-14 17:57:21 +0800

    ""

--- value.py
+++ value.py
@@ -13,21 +13,19 @@ dataset = 'Civil'  # dataset to choose
 
 nsample = 100  # number of generated sample
 
-
-
 path = './save/pm25_validationindex0_20240513_202142/generated_outputs_nsample100.pk'
 with open(path, 'rb') as f:
     samples, all_target, all_evalpoint, all_observed, all_observed_time, scaler, mean_scaler = pickle.load(f)
+
+path_1 = './save/pm25_validationindex0_20240501_081738/generated_outputs_nsample100.pk'
+with open(path_1, 'rb') as f:
+    samples_1, all_target_1, all_evalpoint_1, all_observed_1, all_observed_time_1, scaler_1, mean_scaler_1 = pickle.load(
+        f)
 
 all_target_np = all_target.cpu().numpy()
 all_evalpoint_np = all_evalpoint.cpu().numpy()
 all_observed_np = all_observed.cpu().numpy()
 all_given_np = all_observed_np - all_evalpoint_np
-
-print("samples.shape:", samples.shape)
-print("all_target.shape:", all_target.shape)
-print("all_evalpoint.shape:", all_evalpoint.shape)
-print("all_observed.shape", all_observed.shape)
 
 K = samples.shape[-1]  # feature
 L = samples.shape[-2]  # time length
@@ -40,11 +38,14 @@ if dataset == 'Civil':
     train_mean_cuda = torch.Tensor(train_mean).cuda()
     all_target_np = (all_target_np * train_std + train_mean)
     samples = (samples * train_std_cuda + train_mean_cuda)
+    samples_1 = (samples_1 * train_std_cuda + train_mean_cuda)
 
 qlist = [0.05, 0.25, 0.5, 0.75, 0.95]
 quantiles_imp = []
+quantiles_imp_1 = []
 for q in qlist:
     quantiles_imp.append(get_quantile(samples, q, dim=1) * (1 - all_given_np) + all_target_np * all_given_np)
+    quantiles_imp_1.append(get_quantile(samples_1, q, dim=1) * (1 - all_given_np) + all_target_np * all_given_np)
 
 dataind = 2  # change to visualize a different sample
 
@@ -62,6 +63,11 @@ for k in range(K):
     axes[row][col].plot(range(0, L), quantiles_imp[2][dataind, :, k], color='g', linestyle='solid', label='CSDI')
     axes[row][col].fill_between(range(0, L), quantiles_imp[0][dataind, :, k], quantiles_imp[4][dataind, :, k],
                                 color='g', alpha=0.3)
+
+    axes[row][col].plot(range(0, L), quantiles_imp_1[2][dataind, :, k], color='g', linestyle='solid', label='CSDI')
+    axes[row][col].fill_between(range(0, L), quantiles_imp_1[0][dataind, :, k], quantiles_imp_1[4][dataind, :, k],
+                                color='g', alpha=0.3)
+
     axes[row][col].plot(df.x, df.val, color='b', marker='o', linestyle='None')
     axes[row][col].plot(df2.x, df2.val, color='r', marker='x', linestyle='None')
     if col == 0:
