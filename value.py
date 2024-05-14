6fb516da2aeb4374d8ac97982537b8f58ff7d16b
朱文涛 <20373915@buaa.edu.cn>
2024-05-14 17:59:43 +0800

    ""

--- value.py
+++ value.py
@@ -47,32 +47,35 @@ for q in qlist:
     quantiles_imp.append(get_quantile(samples, q, dim=1) * (1 - all_given_np) + all_target_np * all_given_np)
     quantiles_imp_1.append(get_quantile(samples_1, q, dim=1) * (1 - all_given_np) + all_target_np * all_given_np)
 
-dataind = 2  # change to visualize a different sample
+dataind = 0  # change to visualize a different sample
 
 # plt.rcParams["font.size"] = 16
 fig, axes = plt.subplots(nrows=18, ncols=4, figsize=(24, 72))
 fig.delaxes(axes[-1][-1])
+for dataind in range(6):
+    for k in range(K):
+        df = pd.DataFrame(
+            {"x": np.arange(0, L), "val": all_target_np[dataind, :, k], "y": all_evalpoint_np[dataind, :, k]})
+        df = df[df.y != 0]
+        df2 = pd.DataFrame(
+            {"x": np.arange(0, L), "val": all_target_np[dataind, :, k], "y": all_given_np[dataind, :, k]})
+        df2 = df2[df2.y != 0]
+        row = k // 4
+        col = k % 4
+        axes[row][col].plot(range(0, L), quantiles_imp[2][dataind, :, k], color='g', linestyle='solid', label='CSDI')
+        axes[row][col].fill_between(range(0, L), quantiles_imp[0][dataind, :, k], quantiles_imp[4][dataind, :, k],
+                                    color='g', alpha=0.3)
 
-for k in range(K):
-    df = pd.DataFrame({"x": np.arange(0, L), "val": all_target_np[dataind, :, k], "y": all_evalpoint_np[dataind, :, k]})
-    df = df[df.y != 0]
-    df2 = pd.DataFrame({"x": np.arange(0, L), "val": all_target_np[dataind, :, k], "y": all_given_np[dataind, :, k]})
-    df2 = df2[df2.y != 0]
-    row = k // 4
-    col = k % 4
-    axes[row][col].plot(range(0, L), quantiles_imp[2][dataind, :, k], color='g', linestyle='solid', label='CSDI')
-    axes[row][col].fill_between(range(0, L), quantiles_imp[0][dataind, :, k], quantiles_imp[4][dataind, :, k],
-                                color='g', alpha=0.3)
+        axes[row][col].plot(range(0, L), quantiles_imp_1[2][dataind, :, k], color='gray', linestyle='solid',
+                            label='CSDI')
+        axes[row][col].fill_between(range(0, L), quantiles_imp_1[0][dataind, :, k], quantiles_imp_1[4][dataind, :, k],
+                                    color='gray', alpha=0.3)
 
-    axes[row][col].plot(range(0, L), quantiles_imp_1[2][dataind, :, k], color='g', linestyle='solid', label='CSDI')
-    axes[row][col].fill_between(range(0, L), quantiles_imp_1[0][dataind, :, k], quantiles_imp_1[4][dataind, :, k],
-                                color='g', alpha=0.3)
+        axes[row][col].plot(df.x, df.val, color='b', marker='o', linestyle='None')
+        axes[row][col].plot(df2.x, df2.val, color='r', marker='x', linestyle='None')
+        if col == 0:
+            plt.setp(axes[row, 0], ylabel='value')
+        if row == -1:
+            plt.setp(axes[-1, col], xlabel='time')
 
-    axes[row][col].plot(df.x, df.val, color='b', marker='o', linestyle='None')
-    axes[row][col].plot(df2.x, df2.val, color='r', marker='x', linestyle='None')
-    if col == 0:
-        plt.setp(axes[row, 0], ylabel='value')
-    if row == -1:
-        plt.setp(axes[-1, col], xlabel='time')
-
-plt.savefig("res.png")
+    plt.savefig("res" + str(dataind) + '.png')
